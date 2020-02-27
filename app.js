099303a3702fbf94a3d3dc1c05eb2da4e399ee44
DNSR <good800322@gmail.com>
2020-02-27 15:19:19 +0800

    create routes and introduce seeds to index

--- app.js
+++ app.js
@@ -3,8 +3,15 @@ const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
-const Restaurant = require('./models/restaurantList.js')
 
+//set template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+//introduce static files
+app.use(express.static('public'))
+
+//connect to mongodb
 mongoose.connect('mongodb://localhost/restaurantList', { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 
@@ -19,11 +26,41 @@ db.once('open', () => {
 const Restaurant = require('./models/restaurantList.js')
 
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
+
 
 app.get('/', (req, res) => {
-  res.render('index')
+  Restaurant.find()
+    .lean()
+    .exec((err, restaurant) => {
+      if (err) console.error(err)
+      res.render('index', { restaurant: restaurant })
+    })
+})
+app.get('/restaurants', (req, res) => {
+  Restaurant.find()
+    .lean()
+    .exec((err, restaurant) => {
+      if (err) console.error(err)
+      res.render('index', { restaurant: restaurant })
+    })
+})
+app.get('/restaurants/:id', (req, res) => {
+  res.send('detail of the restaurant')
+})
+app.get('/restaurants/new', (req, res) => {
+  res.send('the page for creating')
+})
+app.post('/restaurants/', (req, res) => {
+  res.send('creating')
+})
+app.get('/restaurants/:id/edit', (req, res) => {
+  res.send('the edit page')
+})
+app.post('/restaurants/:id/edit', (req, res) => {
+  res.send('for edition')
+})
+app.post('/restaurants/:id/delete', (req, res) => {
+  res.send('for delete')
 })
 
 app.listen(port, () => {
--- models/seeds/restaurantListSeeder.js
+++ models/seeds/restaurantListSeeder.js
@@ -2,25 +2,14 @@ const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/restaurantList', { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 const Restaurant = require('../restaurantList.js')
+const restaurantList = require('../../restaurant.json')
 
 db.on('error', () => {
   console.log('mongodb error!')
 })
 db.once('open', () => {
   console.log('mongodb connected')
-
-  for (let i = 0; i < 10; i++) {
-    Restaurant.create({
-      name: 'name' + i,
-      name_en: 'name_en' + i,
-      category: 'category' + i,
-      image: 'image' + i,
-      location: 'location' + i,
-      phone: 'phone' + i,
-      google_map: 'google_map' + i,
-      rating: i,
-      description: 'description' + i
-    })
-  }
+  console.log(...restaurantList.results)
+  Restaurant.create(...restaurantList.results)
   console.log('done')
 })
